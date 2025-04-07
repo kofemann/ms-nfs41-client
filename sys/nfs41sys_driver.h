96112cfb2000e3385f5269dcefd5956a2b226bf7
Cedric Blancher <cedric.blancher@gmail.com>
2025-04-07 15:25:03 +0200

    sys: Increase minimum rsize=/wsize= limit to 8192
    Increase minimum rsize=/wsize= limit to 8192, to make sure it
    can fit a full 4096byte long path.
    
    Signed-off-by: Cedric Blancher <cedric.blancher@gmail.com>
    Signed-off-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

--- sys/nfs41sys_driver.h
+++ sys/nfs41sys_driver.h
@@ -294,7 +294,7 @@ nfs41_updowncall_list upcall, downcall;
 
 
 #define SERVER_NAME_BUFFER_SIZE         1024
-#define MOUNT_CONFIG_RW_SIZE_MIN        1024
+#define MOUNT_CONFIG_RW_SIZE_MIN        8192
 #define MOUNT_CONFIG_RW_SIZE_DEFAULT    (1*1024*1024)
 #define MOUNT_CONFIG_RW_SIZE_MAX        (16*1024*1024)
 #define MAX_SEC_FLAVOR_LEN              12
