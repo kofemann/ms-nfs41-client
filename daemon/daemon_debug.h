e79d83d764b879071e04e52fc747ca1dc4e90d95
Dan Shelton <dan.f.shelton@gmail.com>
2025-08-11 13:12:21 +0200

    daemon: Add {...} brackets to debug macros
    Add {...} brackets to debug macros to avoid confusion in nested
    if()/else statements.
    
    Signed-off-by: Cedric Blancher <cedric.blancher@gmail.com>
    Signed-off-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

--- daemon/daemon_debug.h
+++ daemon/daemon_debug.h
@@ -56,55 +56,65 @@
 #endif /* _WIN64 */
 
 #define EASSERT(exp) \
-    if (!(exp)) { \
-        DWORD d_saved_lasterr = GetLastError(); \
-        eprintf("ASSERTION '%s' in '%s'/%ld failed.\n", \
-            ""#exp"", __FILE__, (long)__LINE__); \
-        SetLastError(d_saved_lasterr); \
+    { \
+        if (!(exp)) { \
+            DWORD d_saved_lasterr = GetLastError(); \
+            eprintf("ASSERTION '%s' in '%s'/%ld failed.\n", \
+                ""#exp"", __FILE__, (long)__LINE__); \
+            SetLastError(d_saved_lasterr); \
+        } \
     }
 #define EASSERT_MSG(exp, msg) \
-    if (!(exp)) { \
-        DWORD d_saved_lasterr = GetLastError(); \
-        eprintf("ASSERTION '%s' in '%s'/%ld failed, msg=", \
-            ""#exp"", __FILE__, (long)__LINE__); \
-        eprintf_out msg ; \
-        SetLastError(d_saved_lasterr); \
+    { \
+        if (!(exp)) { \
+            DWORD d_saved_lasterr = GetLastError(); \
+            eprintf("ASSERTION '%s' in '%s'/%ld failed, msg=", \
+                ""#exp"", __FILE__, (long)__LINE__); \
+            eprintf_out msg ; \
+            SetLastError(d_saved_lasterr); \
+        } \
     }
 #define DASSERT(exp, level) \
-    if (!(exp) && DPRINTF_LEVEL_ENABLED(level)) { \
-        DWORD d_saved_lasterr = GetLastError(); \
-        dprintf_out("ASSERTION '%s' in '%s'/%ld failed.\n", \
-            ""#exp"", __FILE__, (long)__LINE__); \
-        SetLastError(d_saved_lasterr); \
+    { \
+        if ((!(exp)) && DPRINTF_LEVEL_ENABLED(level)) { \
+            DWORD d_saved_lasterr = GetLastError(); \
+            dprintf_out("ASSERTION '%s' in '%s'/%ld failed.\n", \
+                ""#exp"", __FILE__, (long)__LINE__); \
+            SetLastError(d_saved_lasterr); \
+        } \
     }
 #define DASSERT_MSG(exp, level, msg) \
-    if (!(exp) && DPRINTF_LEVEL_ENABLED(level)) { \
-        DWORD d_saved_lasterr = GetLastError(); \
-        dprintf_out("ASSERTION '%s' in '%s'/%ld failed, msg=", \
-            ""#exp"", __FILE__, (long)__LINE__); \
-        dprintf_out msg ; \
-        SetLastError(d_saved_lasterr); \
+    { \
+        if (!(exp) && DPRINTF_LEVEL_ENABLED(level)) { \
+            DWORD d_saved_lasterr = GetLastError(); \
+            dprintf_out("ASSERTION '%s' in '%s'/%ld failed, msg=", \
+                ""#exp"", __FILE__, (long)__LINE__); \
+            dprintf_out msg ; \
+            SetLastError(d_saved_lasterr); \
+        } \
     }
-
 #define DASSERT_IS_VALID_NON_NULL_PTR(exp, level) \
-    if (!DEBUG_IS_VALID_NON_NULL_PTR(exp) && \
-        DPRINTF_LEVEL_ENABLED(level)) { \
-        DWORD d_saved_lasterr = GetLastError(); \
-        dprintf_out("ASSERTION " \
-            "!DEBUG_IS_VALID_NON_NULL_PTR('%s'=0x%p) " \
-            "in '%s'/%ld failed.\n", \
-            ""#exp"", (void *)(exp), __FILE__, (long)__LINE__); \
-        SetLastError(d_saved_lasterr); \
+    { \
+        if (!DEBUG_IS_VALID_NON_NULL_PTR(exp) && \
+            DPRINTF_LEVEL_ENABLED(level)) { \
+            DWORD d_saved_lasterr = GetLastError(); \
+            dprintf_out("ASSERTION " \
+                "!DEBUG_IS_VALID_NON_NULL_PTR('%s'=0x%p) " \
+                "in '%s'/%ld failed.\n", \
+                ""#exp"", (void *)(exp), __FILE__, (long)__LINE__); \
+            SetLastError(d_saved_lasterr); \
+        } \
     }
-
 #define EASSERT_IS_VALID_NON_NULL_PTR(exp) \
-    if (!DEBUG_IS_VALID_NON_NULL_PTR(exp)) { \
-        DWORD d_saved_lasterr = GetLastError(); \
-        eprintf("ASSERTION " \
-            "!DEBUG_IS_VALID_NON_NULL_PTR('%s'=0x%p) " \
-            "in '%s'/%ld failed.\n", \
-            ""#exp"", (void *)(exp), __FILE__, (long)__LINE__); \
-        SetLastError(d_saved_lasterr); \
+    { \
+        if (!DEBUG_IS_VALID_NON_NULL_PTR(exp)) { \
+            DWORD d_saved_lasterr = GetLastError(); \
+            eprintf("ASSERTION " \
+                "!DEBUG_IS_VALID_NON_NULL_PTR('%s'=0x%p) " \
+                "in '%s'/%ld failed.\n", \
+                ""#exp"", (void *)(exp), __FILE__, (long)__LINE__); \
+            SetLastError(d_saved_lasterr); \
+        } \
     }
 
 extern int g_debug_level;
