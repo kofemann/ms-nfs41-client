3b7b3bcf3e25d4ae024f9e5bcca1d5bacba964ca
Roland Mainz <roland.mainz@nrubsig.org>
2025-10-17 17:28:28 +0200

    nfsclientdctl: Add "stopdaemon" subcmd
    Add "stopdaemon" subcmd to ask nfsd.exe to exit.
    
    Signed-off-by: Cedric Blancher <cedric.blancher@gmail.com>
    Signed-off-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

--- nfsclientdctl/nfsclientdctl.c
+++ nfsclientdctl/nfsclientdctl.c
@@ -20,7 +20,7 @@
  */
 
 /*
- * nfsclientdctlexe - Controls for NFS41 client daemon
+ * nfsclientdctl.exe - Controls for NFS41 client daemon
  */
 
 #if ((__STDC_VERSION__-0) < 201710L)
@@ -35,6 +35,10 @@
 #include <stdbool.h>
 #include <string.h>
 
+#include <devioctl.h>
+#include "../include/nfs41_driver.h"
+
+#define EXIT_USAGE (2) /* Traditional UNIX exit code for usage */
 
 #define DPRINTF(level, fmt) \
     if ((level) <= _dprintf_debug_level) { \
@@ -43,9 +47,70 @@
 
 static int _dprintf_debug_level = 0;
 
+static
+HANDLE create_nfs41sys_device_pipe(void)
+{
+    HANDLE pipe;
+    pipe = CreateFileA(NFS41_USER_DEVICE_NAME_A,
+        GENERIC_READ|GENERIC_WRITE,
+        FILE_SHARE_READ|FILE_SHARE_WRITE,
+        NULL,
+        OPEN_EXISTING,
+        0,
+        NULL);
+    return pipe;
+}
+
+static
+void close_nfs41sys_device_pipe(HANDLE pipe)
+{
+    (void)CloseHandle(pipe);
+}
+
+void usage(const char *progname)
+{
+    (void)fprintf(stderr, "Usage: %s [stopdaemon]", progname);
+}
+
+static
+int cmd_stopdaemon(const char *progname)
+{
+    HANDLE pipe;
+    DWORD status;
+    DWORD dstatus;
+    DWORD outbuf_len;
+
+    pipe = create_nfs41sys_device_pipe();
+    if (pipe == INVALID_HANDLE_VALUE) {
+        status = GetLastError();
+        (void)fprintf(stderr,
+            "%s: stopdaemon: "
+            "Unable to open nfs41_driver pipe, lasterr=%d\n",
+            progname,
+            (int)status);
+        return EXIT_FAILURE;
+    }
+    dstatus = DeviceIoControl(pipe, IOCTL_NFS41_STOP,
+        NULL, 0, NULL, 0, &outbuf_len, NULL);
+
+    close_nfs41sys_device_pipe(pipe);
+    return EXIT_SUCCESS;
+}
+
+
 int main(int ac, char *av[])
 {
-    (void)ac;
-    (void)av;
-    return EXIT_SUCCESS;
+    if (ac < 2) {
+        usage(av[0]);
+        return EXIT_USAGE;
+    }
+
+    if (!strcmp(av[1], "stopdaemon")) {
+        return cmd_stopdaemon(av[0]);
+    }
+    else {
+        (void)fprintf(stderr, "%s: Unknown cmd '%s'\n",
+            av[0], av[1]);
+        return EXIT_FAILURE;
+    }
 }
